87af8729c985c7ebcc0722d67fdbe0eb1122e637
Nick "Synth <114616754+synthownsyou@users.noreply.github.com>
2026-09-13 06:41:57 -0700

    Update paths.mm
    Added function to retrieve working folder

--- src/platform/ios/paths.mm
+++ src/platform/ios/paths.mm
@@ -1,7 +1,6 @@
 #include "paths.h"
 
 #include <Foundation/Foundation.h>
-
 #include <SDL.h>
 
 // Modelled after SDL_AndroidGetExternalStoragePath.
@@ -11,18 +10,28 @@ const char* iOSGetDocumentsPath()
 
     if (s_iOSDocumentsPath == NULL) {
         @autoreleasepool {
-            NSArray* array = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+            NSArray* array =
+                NSSearchPathForDirectoriesInDomains(
+                    NSDocumentDirectory,
+                    NSUserDomainMask,
+                    YES);
 
             if ([array count] > 0) {
                 NSString* str = [array objectAtIndex:0];
                 const char* base = [str fileSystemRepresentation];
+
                 if (base) {
                     const size_t len = SDL_strlen(base) + 2;
                     s_iOSDocumentsPath = (char*)SDL_malloc(len);
+
                     if (s_iOSDocumentsPath == NULL) {
                         SDL_OutOfMemory();
                     } else {
-                        SDL_snprintf(s_iOSDocumentsPath, len, "%s/", base);
+                        SDL_snprintf(
+                            s_iOSDocumentsPath,
+                            len,
+                            "%s/",
+                            base);
                     }
                 }
             }
@@ -30,4 +39,78 @@ const char* iOSGetDocumentsPath()
     }
 
     return s_iOSDocumentsPath;
+}
+
+bool iOSBootstrapGameData()
+{
+    @autoreleasepool {
+        NSFileManager* fm = [NSFileManager defaultManager];
+
+        NSArray* paths =
+            NSSearchPathForDirectoriesInDomains(
+                NSDocumentDirectory,
+                NSUserDomainMask,
+                YES);
+
+        if ([paths count] == 0) {
+            return false;
+        }
+
+        NSString* documents = [paths objectAtIndex:0];
+
+        NSString* masterDestination =
+            [documents stringByAppendingPathComponent:@"master.dat"];
+
+        // Already installed.
+        if ([fm fileExistsAtPath:masterDestination]) {
+            return true;
+        }
+
+        NSString* resourcePath =
+            [[NSBundle mainBundle] resourcePath];
+
+        NSString* sourceRoot =
+            [resourcePath stringByAppendingPathComponent:@"GameData"];
+
+        NSArray* items = @[
+            @"master.dat",
+            @"critter.dat",
+            @"data"
+        ];
+
+        for (NSString* item in items) {
+            NSString* source =
+                [sourceRoot stringByAppendingPathComponent:item];
+
+            NSString* destination =
+                [documents stringByAppendingPathComponent:item];
+
+            if (![fm fileExistsAtPath:source]) {
+                NSLog(@"FOCE bootstrap: missing %@", source);
+                return false;
+            }
+
+            // Remove a partial previous copy.
+            if ([fm fileExistsAtPath:destination]) {
+                NSError* removeError = nil;
+                [fm removeItemAtPath:destination
+                               error:&removeError];
+            }
+
+            NSError* copyError = nil;
+
+            if (![fm copyItemAtPath:source
+                             toPath:destination
+                              error:&copyError]) {
+                NSLog(
+                    @"FOCE bootstrap: failed copying %@: %@",
+                    item,
+                    copyError);
+
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
